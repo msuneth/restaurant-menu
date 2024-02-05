1aa7f2b8647170cc30b506bbb0abda6f5cf018aa
msuneth <msuneth@gmail.com>
2024-02-05 12:57:10 +0530

    show menu items, desc from database

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Item
+
+
 # Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    search_fields = ("meal", "description")
+    list_filter = ("status",)
+
+
+admin.site.register(Item, MenuItemAdmin)

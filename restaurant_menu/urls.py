93ac9b818e6e3d6cda92088f03c73836270cac04
msuneth <msuneth@gmail.com>
2024-02-05 18:45:46 +0530

    base html, bootstrap

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name="Home")
+    path('', views.MenuList.as_view(), name="home"),
+    path('item/<int:pk>/', views.MenuItemDetails.as_view(), name="menu_item")
 ]

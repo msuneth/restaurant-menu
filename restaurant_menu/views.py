0235fed7c6e06a24ad989c9b024939b7ac600154
msuneth <msuneth@gmail.com>
2024-02-05 11:55:10 +0530

    load static data from backend to front end

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,6 +7,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {"meals":"Pizza"}
+        return context
+
 
 class MenuItemDetails(generic.DetailView):
     model = Item
